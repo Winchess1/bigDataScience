4a5dc55e863163009a27061df5c1fa507963431a
Winchess1 <ikneazevmd@gmail.com>
2019-04-11 13:25:44 -0500

    Version 1.0 ready
    Error message were added.
    Fixed Performance from 100 to 50 ms.

--- index2.js
+++ index2.js
@@ -16,6 +16,8 @@ var data = [];
 var key = [];
 var counter = 0;
 
+
+//requesting for Stock list
 const allStocks = {
     url: `https://api.iextrading.com/1.0/ref-data/symbols`,
     json: true,
@@ -23,74 +25,35 @@ const allStocks = {
     requestCert: true,
     agent: false
 }
-request(allStocks, function (error, response, body) {
-    console.log("\tgrabbing and extracting the Stocks from API");
-    collection = _.map(_.filter(body, function (data) {
-        return data['type'] !== 'et' && data['type'] !== 'N/A';
-    }),
-        function (data) {
-            return data.symbol
-        });
-    /* 
-           1. f1 = taking the 1st symbol
-           2. f2 = checking if file exist
-           3. create viriable to save the path
-           4. save the file 
-                     */
-
-    //Awesome timer for Arrays
-    var interval = setInterval(function () {
-        count(collection.pop());
-        if (collection.length == 0) {
-            clearInterval(interval);
-            console.log('\ttask us done!');
-            console.log('\ttotal valid stocks '+ counter)
-        }
-    }, 100);
-
+request(allStocks, function (error, res, body) {
+    if (!error && res.statusCode == 200) {
+        console.log("\tgrabbing and extracting the Stocks from API");
+        collection = _.map(_.filter(body, function (data) {
+            return data['type'] !== 'et' && data['type'] !== 'N/A';
+        }),
+            function (data) {
+                return data.symbol
+            });
+        //Awesome timer for Arrays
+        var interval = setInterval(function () {
+            count(collection.pop());
+            if (collection.length == 0) {
+                clearInterval(interval);
+                console.log('\ttask was done!');
+                console.log('\ttotal valid stocks = ' + counter)
+            }
+        }, 50);
+    } else console.log("Something wrong with API " + error)
 });
-//Symbol checker
-var symbol = function (symbolExtractor) {
-    var symbolPath = symbolExtractor.charAt(0);
-    return symbolPath;
-}
-
-//creating a directories
-var dirCheck = function (fileSymbol) {
-
-    if (!(fs.existsSync(path.join(date, fileSymbol)))) {
-console.log('\tno directory')
-        mkdirp(path.join(date, fileSymbol), function (err) {
-            if (err) console.error(err)
-            else console.log('\t\t' + fileSymbol + ' folder is created')
-        });
-
-    }
-}
-//creating files
-var fileCreator = function (collection, temp,jsonFile) {
-    if (!(fs.existsSync(path.basename(path.join(date, temp, collection + '.json'))))) {
-        console.log('\t Writing the file! ' + collection);
-        fs.writeFile(path.join(date, temp, collection + '.json'),JSON.stringify(jsonFile), function (err) {
-            console.log('\t\t' + collection + 'file saved')
-            if (err) throw err;
-
-        });
-
-    }
-
-}
 
 function count(collection) {
-
     request('https://finviz.com/quote.ashx?t=' + collection, (error, res, html) => {
-        console.log(collection + ' Stock in action')
-        if (!error && res.statusCode == 200) {         
-
+        if (!error && res.statusCode == 200) {
+            console.log(collection + ' Stock in action')
             const $ = cheerio.load(html);
 
             let stockNameFisrtLetterTemp = collection.charAt(0);
-            console.log('Collecting info')
+
             $('.table-dark-row .snapshot-td2-cp').each((i, el) => {
                 key[i] = $(el).text();
 
@@ -108,18 +71,46 @@ function count(collection) {
             var temp = symbol(collection);
             dirCheck(temp);
 
-            fileCreator(collection, temp,jsonFile);
+            fileCreator(collection, temp, jsonFile);
 
             counter++;
-            console.log(counter);
 
-
-
-
-        }
-
+        } else console.log('\t' + collection + 'Stock not exist in Finviz')
 
     }
     )
+
+}
+
+//Symbol checker
+var symbol = function (symbolExtractor) {
+    var symbolPath = symbolExtractor.charAt(0);
+    return symbolPath;
+}
+
+//creating a directories
+var dirCheck = function (fileSymbol) {
+
+    if (!(fs.existsSync(path.join(date, fileSymbol)))) {
+
+        mkdirp(path.join(date, fileSymbol), function (err) {
+            if (err) console.error(err)
+            else console.log('\t\t' + fileSymbol + ' folder is created')
+        });
+
+    } else (console.log(fileSymbol + ' Directory exist'))
+}
+//creating files
+var fileCreator = function (collection, temp, jsonFile) {
+    if (!(fs.existsSync(path.join(date, temp, collection + '.json')))) {
+        console.log(path.join(date, temp, collection + '.json'));
+        console.log('\t Writing the file! ' + collection);
+        fs.writeFile(path.join(date, temp, collection + '.json'), JSON.stringify(jsonFile), function (err) {
+            console.log('\t\t' + collection + ' file saved')
+            if (err) throw err;
+
+        });
+
+    } else (console.log(collection + 'File exist'))
 
 }
